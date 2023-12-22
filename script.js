fdda5407812af6460823ff9af461e43bc56accbd
Andrei Demichev <andyuson24@gmail.com>
2023-12-22 19:00:10 +0200

    выполнил сложное задание

--- script.js
+++ script.js
@@ -2,33 +2,51 @@ const isNumber = (num) => {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-function guessNumber (myNumber) {
+function guessNumber (myNumber, tryCount) {
+
     function questionsAboutNumber () {
+        const count = ' осталось ' + tryCount + ' попыток';
         const askNumber = prompt ('Угадай число от 1 до 100');
-        
+
+        if (tryCount >= 0){
+            tryCount--
+        }
+
+        if(tryCount === -1){
+            if(confirm('попытки закончились, хотите сыграть еще?')){
+                guessNumber(10,3);
+            }
+            return;
+        }
+
         if (askNumber === null){
-            alert('Игра окончена');
+            alert('Игра окончена' );
             return 0;
         }
+
         if (askNumber > myNumber) {
-            alert('Загаданное число меньше');
+            alert('Загаданное число меньше ' + count);
             questionsAboutNumber();
         }
+
         if (askNumber < myNumber){
-            alert('Загаданное число больше');
+            alert('Загаданное число больше ' + count);
             questionsAboutNumber();
         }
+
         if (!isNumber(askNumber)){
-            alert('Введи число!');
+            alert('Введи число! ' + count);
             questionsAboutNumber();
         }
+
         if (askNumber === myNumber){
-            confirm('Поздравляю, Вы угадали!!!');
+            if(confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')){
+                guessNumber(12,3);
+            }
+            return;
         }
         
-    }
-
+    }   
     questionsAboutNumber()
 }
-
-guessNumber(10)
+guessNumber(15, 3);
